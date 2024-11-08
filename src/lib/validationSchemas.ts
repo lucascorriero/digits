80f758a4902ce905bb54e00790a2cefa2c565c55
lucascorriero <lucascorriero@gmail.com>
2024-11-07 22:44:01 -1000

    dnf

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -7,6 +7,26 @@ export const AddStuffSchema = Yup.object({
   owner: Yup.string().required(),
 });
 
+export const AddContactSchema = Yup.object({
+  id: Yup.number().required(),
+  firstName: Yup.string().required(),
+  lastName: Yup.string().required(),
+  address: Yup.string().required(),
+  image: Yup.string().required(),
+  description: Yup.string().required(),
+  owner: Yup.string().required(),
+});
+
+export const EditContactSchema = Yup.object({
+  id: Yup.number().required(),
+  firstName: Yup.string().required(),
+  lastName: Yup.string().required(),
+  address: Yup.string().required(),
+  image: Yup.string().required(),
+  description: Yup.string().required(),
+  owner: Yup.string().required(),
+});
+
 export const EditStuffSchema = Yup.object({
   id: Yup.number().required(),
   name: Yup.string().required(),
